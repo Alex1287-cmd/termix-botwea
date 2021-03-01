0a9170fd43de18c1e27b2224665ce4328bcee317
Alex1287-cmd <75934725+Alex1287-cmd@users.noreply.github.com>
2021-03-01 10:45:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -101,9 +101,9 @@ lolKey = up.LolHumanKey
 //--Kontak
 const vcard = 'BEGIN:VCARD\n'
 + 'VERSION:3.0\n'
-+ 'FN:Mrf.zvx\n' // Nama
++ 'FN:AL1287\n' // Nama
 + 'ORG:Lexa bot;\n' // Nama bot
-+ 'TEL;type=CELL;type=VOICE;waid=6282223014661:+62 822-2301-4661\n' // Nomor bot
++ 'TEL;type=CELL;type=VOICE;waid=628881393308:+62 888-1393-308\n' // Nomor bot
 + 'END:VCARD' 
 
 
